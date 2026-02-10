59f47bbed1133d5382d2d49ffc63de8971a3bb02
Smiksha <smiksha@sqelabs.com>
2026-02-10 11:07:02 +0530

    login flow of HRMS

---
+++ tests/login.test.ts
@@ -0,0 +1,17 @@
+ import{chromium, test} from "@playwright/test"
+ test("Login test demo",async()=>{
+     const browser= await chromium.launch({
+        headless:false
+ }); //launch browser instance stored in browser
+     const context= await browser.newContext();
+     const page =await context.newPage();
+
+     await page.goto("https://topuptalent.com/")
+    await page.hover('[name="email"]');
+    await page.click('[name="email"]',{delay:200});
+    await page.type('input[name="email"]',"vishal.thakur@caeliusconsulting.com",{delay:200});
+    await page.type('input[name="password"]',"Test@123",{delay:200});
+     await page.click('.theme-button');
+     await page.waitForTimeout(5000);
+;
+ })
